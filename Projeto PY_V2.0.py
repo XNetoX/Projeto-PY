69f244632caac7da5369d9a597665d3fa39d7ea1
XNetox <programanetox@gmail.com>
2021-09-29 12:57:36 -0300

    Versão 2.0 Windows

---
+++ Projeto PY_V2.0.py
@@ -0,0 +1,17 @@
+#! python
+"""
+Windows
+Calcular a area de uma circunferencia e imprimir o resultado
+pi=3.1415
+raio=15.3
+a=pi*raio**2
+"""
+
+
+# variaveis
+pi = 3.1415
+raio = 15.3
+a = pi*raio**2
+
+# resultado
+print("Area da circunferencia:", a)
